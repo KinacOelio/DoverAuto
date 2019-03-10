21d28c635f88899b1b2c267c97c4369d1bcc95eb
Nathan Brackenbury <ntw6232@wmich.edu>
2019-03-10 15:52:44 -0400

    improved locations page

--- javaScript/master.js
+++ javaScript/master.js
@@ -1,4 +1,6 @@
 	
 $("#navbar1").load("navbar.html");
 
+function dropFunc1() {document.getElementById("myDropdown1").classList.toggle("show");} function dropFunc2() {document.getElementById("myDropdown2").classList.toggle("show");} function dropFunc3() {document.getElementById("myDropdown3").classList.toggle("show");} function dropFunc4() {document.getElementById("myDropdown4").classList.toggle("show");} function dropFunc5() {document.getElementById("myDropdown5").classList.toggle("show");}
+
 
