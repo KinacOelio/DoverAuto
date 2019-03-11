54c70eeb80d0ae0f456ca8282d13d772fbffe08e
Nathan Brackenbury <ntw6232@wmich.edu>
2019-03-10 20:10:18 -0400

    changed navbar load to remote link

--- javaScript/master.js
+++ javaScript/master.js
@@ -1,5 +1,5 @@
 	
-$("#navbar1").load("navbar.html");
+$("#navbar1").load("https://raw.githubusercontent.com/KinacOelio/WebTechMidTerm/master/html/navbar.html");
 
 function dropFunc1() {document.getElementById("myDropdown1").classList.toggle("show");} function dropFunc2() {document.getElementById("myDropdown2").classList.toggle("show");} function dropFunc3() {document.getElementById("myDropdown3").classList.toggle("show");} function dropFunc4() {document.getElementById("myDropdown4").classList.toggle("show");} function dropFunc5() {document.getElementById("myDropdown5").classList.toggle("show");}
 
