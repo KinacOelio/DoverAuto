b2e78004fa9fab9804d783c5b9ed554a134db57f
Nathan Brackenbury <ntw6232@wmich.edu>
2019-04-12 11:30:55 -0400

    messing around with js

--- html/catalog.php
+++ html/catalog.php
@@ -38,21 +38,15 @@ global $pdo;
 	<link rel="stylesheet" media="screen and (min-width: 1200px)" href="../css/masterLarge.css">
 	<link href="../css/catalog.css" rel="stylesheet">
 	<script type="text/javaScript" src="../javaScript/jquery-3.3.1.js"></script>
-
 </head>
 
-
 <body id="OR">
-<div class="container">
-
- <div id="navbar1"></div>
- <script>
-</script>
+<div id="navbar"></div>
 
 <div class="container">
-<div class="row catHeader">
-<div class="col"><h1>Vehichle Catalog</h1></div>
-</div>
+	<div class="row catHeader">
+		<div class="col"><h1>Vehichle Catalog</h1></div>
+	</div>
 <hr>
 	<div class="row catHeader">
 		<div class="col">
@@ -87,16 +81,9 @@ global $pdo;
 		</div>
 </div>
 
-</div>	
-
-
 <div id="footer"></div>
-<script>$("#footer").load("https://raw.githubusercontent.com/KinacOelio/WebTechMidTerm/master/html/footer.html");</script> 
-
 
 <script src="../javaScript/master.js"></script>
-
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
--- javaScript/master.js
+++ javaScript/master.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 	
-	$("#navbar1").load("https://raw.githubusercontent.com/KinacOelio/WebTechMidTerm/master/html/navbar.html");
+	$("#navbar").load("https://raw.githubusercontent.com/KinacOelio/WebTechMidTerm/master/html/navbar.html");
+	$("#footer").load("https://raw.githubusercontent.com/KinacOelio/WebTechMidTerm/master/html/footer.html");
 
 	var locationList = document.getElementById("locations");
 	
 	locationList.addEventListener("click", function(e){
-		console.log(e.target);
-		var button = e.target;
-		button.classList.toggle("active");
-		var hours = document.querySelector(".active + .dropdown-content");
-		console.log(hours);
-		hours.classList.toggle("show");
-		button.classList.toggle("active");
+		$(this + ".dropdown-content").hide();
+	
+		//console.log(e.target);
+		//var button = e.target;
+		//button.classList.toggle("active");
+		//var hours = document.querySelector(".active + .dropdown-content");
+		//console.log(hours);
+		//hours.classList.toggle("show");
+		//button.classList.toggle("active");
 	});
 
  
